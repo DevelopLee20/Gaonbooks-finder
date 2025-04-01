e976f935b39bf492bfe739728f36b8b0f3a25215
LeeInGyu <developLee20@gmail.com>
2025-04-01 17:02:23 +0900

    feat: 엑셀파일 연결 기능 구현 (#1)
    Closes: #1

--- fullstack/backend/app/main.py
+++ fullstack/backend/app/main.py
@@ -1,13 +1,29 @@
 from fastapi import FastAPI
 import psutil
+import sys
+import os
+from app.books_init import Books
 
-app = FastAPI()
+# init
+books = Books()
+soldout_list = books.get_soldout_list()
+dataframe = books.get_dataframe()
+excel_file_name = books.get_excel_file_name()
+
+# app
+app = FastAPI(
+    title="Gaonbooks",
+)
 
 @app.get("/")
 async def default_page():
     for _, snics in psutil.net_if_addrs().items():
         for snic in snics:
             if snic.family.name == "AF_INET" and not snic.address.startswith("127."):
-                return {"ip": snic.address, "port": "8000", "address": f"http://{snic.address}:8000"}
+                return {
+                    "address": f"http://{snic.address}:8000",
+                    "file_name": excel_file_name,
+                    "soldout_list_size": len(soldout_list),
+                }
 
     return {"detail": "default_page_error"}
