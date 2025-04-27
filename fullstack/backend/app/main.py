749fb858fd07043746d58bd0fbb458a495b23eba
LeeInGyu <developLee20@gmail.com>
2025-04-27 23:42:48 +0900

    Feat: 책 위치 검색 API 구현 (#2)
    Closes: #2

--- fullstack/backend/app/main.py
+++ fullstack/backend/app/main.py
@@ -1,12 +1,11 @@
+import json
+
 import psutil
 from app.books_init import Books
 from fastapi import FastAPI
 
 # init
 books = Books()
-soldout_list = books.get_soldout_list()
-dataframe = books.get_dataframe()
-excel_file_name = books.get_excel_file_name()
 
 # app
 app = FastAPI(
@@ -21,8 +20,25 @@ async def default_page():
             if snic.family.name == "AF_INET" and not snic.address.startswith("127."):
                 return {
                     "address": f"http://{snic.address}:8000",
-                    "file_name": excel_file_name,
-                    "soldout_list_size": len(soldout_list),
+                    "file_name": books.latest_file,
+                    "soldout_list_size": len(books.soldout_list),
                 }
 
     return {"detail": "default_page_error"}
+
+
+@app.get("/find/{input_book_name}/")
+async def find_book(input_book_name: str):
+    user_input = input_book_name.strip()
+
+    if user_input == "":
+        return
+
+    filtered_df = books.dataframe[
+        books.dataframe["도서명"].str.contains(user_input, case=False, na=False)
+    ].copy()
+
+    # orient: 리스트로 출력, force_ascii: 한글 출력 가능
+    json_df = filtered_df.to_json(orient="records", force_ascii=False)
+
+    return json.loads(json_df)
